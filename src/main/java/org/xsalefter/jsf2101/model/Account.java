0e32ed26469c72acdfdb789d160241be0f239df9
xsalefter <xsalefter@gmail.com>
2012-02-29 22:37:43 +0700

    Implements java.io.Serializable and hashcode() and equals(obj);

--- src/main/java/org/xsalefter/jsf2101/model/Account.java
+++ src/main/java/org/xsalefter/jsf2101/model/Account.java
@@ -1,9 +1,13 @@
 package org.xsalefter.jsf2101.model;
 
+import java.io.Serializable;
+
 /**
  * @author xsalefter
  */
-public class Account {
+public class Account implements Serializable {
+
+	private static final long serialVersionUID = 5017657178494749952L;
 
 	private Long id;
 	private String name;
@@ -40,6 +44,31 @@ public class Account {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Account))
+			return false;
+		Account other = (Account) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 	
 }
